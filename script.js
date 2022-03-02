3fd1f208ef537f73690a2e5fbe5859045d35a1b2
Talha <tkz9696@gmail.com>
2022-03-02 21:48:24 +0500

    Fixed linter errors

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
