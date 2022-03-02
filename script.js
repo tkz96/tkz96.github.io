8013a69bed82f6b7c35eecd0f60fe1226a32c838
Kartikay <kartiksrivastava19@gmail.com>
2022-03-02 16:11:32 +0100

    Add eslint and Fix linters errors

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-//stay away from toggle in the future
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+// stay away from toggle in the future
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
 // document.querySelectorAll(".nav-item").forEach(n => addEventListener("click", () => {
