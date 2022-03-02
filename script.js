9b73da64d77a6adffb4d74765ec37647729c0669
talha <tkz9696@gmail.com>
2022-03-02 21:24:39 +0500

    fixed everything

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
-// stay away from toggle in the future
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-// document.querySelectorAll(".nav-item").forEach(n => addEventListener("click", () => {
-//     hamburger.classList.remove("active");
-//     navMenu.classList.remove("active");
-// }));
+document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
