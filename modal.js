27d87c8f31df046a3a5e9b0d8a48de786d05df57
talha <tkz9696@gmail.com>
2022-03-05 02:39:36 +0500

    Made dummy objects

--- modal.js
+++ modal.js
@@ -1,9 +1,90 @@
 const overlay = document.querySelector("#overlay");
+const modalImg = document.querySelectorAll("#");
+const modalTxt = document.querySelectorAll("#");
+
+const details = [
+    {
+        name: 'Tonic',
+        desc: 'Lorem Ipsum',
+        imageSrc: '',
+        featuredImage: '',
+        technologies: '',
+        link_LiveVersion: '',
+        link_Source: ''
+    },    
+    {
+        name: 'Mutli-Post Stories ',
+        desc: 'Lorem Ipsum',
+        imageSrc: '',
+        featuredImage: '',
+        technologies: '',
+        link_LiveVersion: '',
+        link_Source: ''
+    },
+    {
+        name: 'Tonic',
+        desc: 'Lorem Ipsum',
+        imageSrc: '',
+        featuredImage: '',
+        technologies: '',
+        link_LiveVersion: '',
+        link_Source: ''
+    },
+    {
+        name: 'Multi-Post Stories ',
+        desc: 'Lorem Ipsum',
+        imageSrc: '',
+        featuredImage: '',
+        technologies: '',
+        link_LiveVersion: '',
+        link_Source: ''
+    }
+];
 
 document.querySelector("#show-modal-btn").addEventListener("click", () => {
     overlay.style.display = "block";
+
 });
 
 document.querySelector("#close-modal-btn").addEventListener("click", () => {
     overlay.style.display = "none";
 });
+
+
+
+        
+/*
+1. name <h2>
+2. description
+3. featured image
+4. technologies
+5. link to live version
+6. link to source.
+*/
+
+/*
+1. name <h2>
+2. description
+3. featured image
+4. technologies
+5. link to live version
+6. link to source.
+*/
+
+/*
+1. name <h2>
+2. description
+3. featured image
+4. technologies
+5. link to live version
+6. link to source.
+*/
+
+/*
+1. name <h2>
+2. description
+3. featured image
+4. technologies
+5. link to live version
+6. link to source.
+*/
