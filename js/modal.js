2191081c1ed9855fbdbf3f0ed0f89db877cbb44d
talha <tkz9696@gmail.com>
2022-03-05 22:12:54 +0500

    Open and close both working

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,20 @@
 /* eslint-disable linebreak-style */
-// import modalData from './modal-data.js';
+import modalData from './modal-data.js';
 const overlay = document.querySelector('#overlay');
 
 document.querySelector('#close-modal-btn').addEventListener('click', () => {
   overlay.style.display = 'none';
 });
 
+document.querySelectorAll('.see-project').forEach((e) => {
+  e.addEventListener('click', () => {
+    overlay.style.display = 'block';
+  });
+});
+
+function myFunction(i) {
+  modalData[i];
+}
+
 // i is the button id
-// modalData[i] -> fill up the modal
+// modalData[i] -> fill up the modalsee-project
