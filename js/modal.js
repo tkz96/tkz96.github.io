18508c8a9338d1587bce0c253835aca55a7aa3fa
talha <tkz9696@gmail.com>
2022-03-05 20:52:04 +0500

    Close button working

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,10 @@
-import modalData from './modal-data.js';
+/* eslint-disable linebreak-style */
+// import modalData from './modal-data.js';
+const overlay = document.querySelector('#overlay');
+
+document.querySelector('#close-modal-btn').addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
+
+// i is the button id
+// modalData[i] -> fill up the modal
