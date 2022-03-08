4e827f172fc6f5c7762336a41f3a627993e76345
talha <tkz9696@gmail.com>
2022-03-08 21:30:54 +0500

    Need to fix styling and add proper content

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,44 @@
+const modalData = [
+  {
+    name: 'Tonic',
+    desc: 'Akarr',
+    imageSrc: '',
+    technologies: ['css'],
+    link_live: 'https://tkz96.github.io/',
+    link_github: 'https://github.com/tkz96/tkz96.github.io',
+  },
+  {
+    name: 'Multi-Post Stories',
+    desc: 'Bakkarr',
+    imageSrc: '',
+    technologies: [],
+    link_live: 'https://tkz96.github.io/',
+    link_github: 'https://github.com/tkz96/tkz96.github.io',
+  },
+  {
+    name: 'Tonic',
+    desc: 'Bambay',
+    imageSrc: '',
+    technologies: [],
+    link_live: 'https://tkz96.github.io/',
+    link_github: 'https://github.com/tkz96/tkz96.github.io',
+  },
+  {
+    name: 'Multi-Post Stories',
+    desc: 'Bo',
+    imageSrc: '',
+    technologies: ['html', 'js'],
+    link_live: 'https://tkz96.github.io/',
+    link_github: 'https://github.com/tkz96/tkz96.github.io',
+  },
+];
+
 const openModal = () => {
   const overlay = document.getElementById('overlay');
-  overlay.style.display = 'block';
+  overlay.style.display = 'flex';
 };
 
+// eslint-disable-next-line no-unused-vars
 const closeModal = () => {
   const overlay = document.getElementById('overlay');
   overlay.style.display = 'none';
@@ -20,6 +56,6 @@ const openModalWithData = (i) => {
 
   tagsContainer.innerHTML = data.technologies.map((tech) => `<li class="tag">${tech}</li>`).join('');
 
-  navMenu.classList.remove('active');
+  //navMenu.classList.remove('active');
   openModal();
 };
